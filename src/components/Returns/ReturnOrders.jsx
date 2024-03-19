02c172a3fb03aa16e3fadb7471f51c8245de6d35
Mashal <zyadomar112@gmail.com>
2024-03-19 04:59:57 +0200

    update return

--- src/components/Returns/ReturnOrders.jsx
+++ src/components/Returns/ReturnOrders.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ReturnOrders.css";
+import getAllReturn from "../../api/getAllReturn.api";
+import returnSearch from "../../api/returnSearch.api";
+import deleteReturn from "../../api/deleteReturn.api";
 const ReturnOrders = () => {
-  const openDeleteReturn = () => {
+  const [returnloading, setReturnLoading] = useState(false);
+  const [returnError, setReturnError] = useState("");
+  const [allReturnsSearch, setAllReturnsSearch] = useState("");
+  const [allReturns, setAllReturns] = useState([]);
+  const [returnCode, setReturnCode] = useState("");
+  const [requestId, setRequestId] = useState("");
+  const [returnSearchLoading, setReturnSearchLoading] = useState(false);
+  const [returnSearchError, setReturnSearchError] = useState("");
+  const [returnDeleteLoading, setReturnDeleteLoading] = useState(false);
+  const [returnDeleteError, setReturnDeleteError] = useState("");
+
+  useEffect(() => {
+    getAllReturnsRequest(setAllReturns);
+  }, []);
+  const openDeleteReturn = (requestId) => {
+    setRequestId(requestId);
     document
       .querySelector(".delete_return_popup")
       .classList.replace("d-none", "d-flex");
@@ -11,48 +29,96 @@ const ReturnOrders = () => {
       .querySelector(".delete_return_popup")
       .classList.replace("d-flex", "d-none");
   };
+  const getAllReturnsRequest = () => {
+    getAllReturn(setAllReturns, setReturnLoading, setReturnError);
+  };
+  if (returnloading) {
+    <span className="loader"></span>;
+  }
+  const handleReturnSearch = () => {
+    returnSearch(
+      setReturnSearchLoading,
+      setReturnSearchError,
+      setAllReturns,
+      returnCode
+    );
+  };
+  const handleDelete = () => {
+    deleteReturn(
+      requestId,
+      setReturnDeleteLoading,
+      setReturnDeleteError,
+      setAllReturns
+    );
+  };
   return (
     <div className="return_container">
       <div className="return_box">
         <div className="search_box">
-          <input type="text" placeholder="بحث" />
-          <button>بحث</button>
+          <input
+            type="text"
+            placeholder="بحث"
+            value={returnCode}
+            onChange={(e) => {
+              setReturnCode(e.target.value);
+              if (e.target.value == "") {
+                getAllReturnsRequest();
+              }
+            }}
+          />
+          <button onClick={handleReturnSearch}>بحث</button>
         </div>
-        <div className="review_return_table">
-          <table>
-            <tr>
-              <th>اسم العميل</th>
-              <th>رقم الهاتف</th>
-              <th>رقم الطلب</th>
-              <th>سبب الاسترجاع</th>
-              <th>حذف</th>
-            </tr>
-            <tr>
-              <td>زياد احمد محمد مشعل</td>
-              <td>1234567891463</td>
-              <td>12345678</td>
-              <td className="problem">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea
-                amet modi voluptate ex neque magni veritatis velit, suscipit
-                natus blanditiis debitis, sint minima veniam facere dignissimos
-                eius aut dolores nemo!
-              </td>
-              <td className="delete">
-                <button className="delete_return" onClick={openDeleteReturn}>
-                  حذف
-                </button>
-              </td>
-            </tr>
-          </table>
-        </div>
+        {returnSearchLoading ? (
+          <div className="search_box_loader">
+            <span className="loader"></span>
+          </div>
+        ) : returnSearchError ? (
+          <p>{returnSearchError}</p>
+        ) : (
+          <div className="review_return_table">
+            <table>
+              <tr>
+                <th>اسم العميل</th>
+                <th>اسم المنتج</th>
+                <th>رقم الهاتف</th>
+                <th>رقم الطلب</th>
+                <th>سبب الاسترجاع</th>
+                <th>حذف</th>
+              </tr>
+              {allReturns.map((item) => {
+                return (
+                  <tr>
+                    <td className="userName">{item.userID.userName}</td>
+                    <td className="productName">{item.productName}</td>
+                    <td>{item.phoneNumber}</td>
+                    <td>{item.requestCode}</td>
+                    <td className="problem">{item.message}</td>
+                    <td className="delete">
+                      <button
+                        className="delete_return"
+                        onClick={() => openDeleteReturn(item._id)}
+                      >
+                        حذف
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </table>
+          </div>
+        )}
+
         {/* delete return */}
         <div className="delete-coupon-popup delete_return_popup d-none ">
           <h3>هل تريد حذف هذا المرتجع ؟</h3>
-          <p>Error is here</p>
+          <p>{returnDeleteError}</p>
           <div className="delete-btns">
-            <button>
-              حذف
-              {/* {delteCouponLoading ? <span className="loaderAdd"></span> : "حذف"} */}
+            <button onClick={handleDelete}>
+              {returnDeleteLoading ? (
+                <span className="loaderAdd"></span>
+              ) : (
+                "حذف"
+              )}
             </button>
             <button onClick={closeReturnDelete}>لا</button>
           </div>
